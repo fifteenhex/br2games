192257e1120ef72ddb6bd9cb0cbb60e3504f89e2
Daniel Palmer <daniel@0x0f.com>
2022-02-06 12:06:23 +0900

    scummvm: fix install path scummvm: delete docs from target

--- package/scummvm/scummvm.mk
+++ package/scummvm/scummvm.mk
@@ -10,28 +10,33 @@ SCUMMVM_SITE = https://github.com/scummvm/scummvm/archive/refs/tags
 SCUMMVM_DEPENDENCIES = sdl
 SCUMMVM_LICENSE = GPL-3.0
 
-define SCUMMVM_BUILD_CMDS
-	cd $(@D) && PATH=$(HOST_DIR)/bin:$(PATH)							\
-		PKG_CONFIG_LIBDIR=$(TARGET_DIR)/usr/lib/pkgconfig/				\
-		./configure									\
-		--with-sdl-prefix=$(STAGING_DIR)/usr/bin/					\
-		--prefix=$(TARGET_DIR)								\
-		--backend=sdl									\
-		--host=arm-buildroot-linux-gnueabihf						\
-		--disable-cloud									\
-		--disable-lua									\
-		--disable-debug									\
-		--disable-mt32emu								\
-		--disable-all-engines								\
-		--disable-nuked-opl								\
-		--no-builtin-resources								\
-		--enable-release								\
+define SCUMMVM_CONFIGURE_CMDS
+	cd $(@D) && PATH=$(HOST_DIR)/bin:$(PATH)			\
+		PKG_CONFIG_LIBDIR=$(TARGET_DIR)/usr/lib/pkgconfig/	\
+		./configure						\
+		--with-sdl-prefix=$(STAGING_DIR)/usr/bin/		\
+		--prefix=$(TARGET_DIR)/usr/				\
+		--backend=sdl						\
+		--host=arm-buildroot-linux-gnueabihf			\
+		--disable-cloud						\
+		--disable-lua						\
+		--disable-debug						\
+		--disable-mt32emu					\
+		--disable-all-engines					\
+		--disable-nuked-opl					\
+		--no-builtin-resources					\
+		--enable-release					\
 		--enable-engine=scumm
+endef
+
+define SCUMMVM_BUILD_CMDS
 	PATH=$(HOST_DIR)/bin:$(PATH) make -C $(@D) -j$(NCPUS)
 endef
 
 define SCUMMVM_INSTALL_TARGET_CMDS
 	PATH=$(HOST_DIR)/bin:$(PATH) make -C $(@D) install
+	rm -r $(TARGET_DIR)/usr/share/doc/scummvm
+
 endef
 
 $(eval $(generic-package))
