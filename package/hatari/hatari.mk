c396523689d264ef37025ac29394bc49a8f2165e
Daniel Palmer <daniel@0x0f.com>
2022-02-13 18:25:40 +0900

    package: add hatari

---
+++ package/hatari/hatari.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# hatari
+#
+################################################################################
+
+HATARI_VERSION = 0b353ce12d8e78a812cb204682a20f2e260957a8
+HATARI_SITE = https://github.com/hatari/hatari.git
+HATARI_SITE_METHOD = git
+HATARI_DEPENDS = sdl2
+HATARI_LICENSE = GPL-2.0
+
+$(eval $(cmake-package))
