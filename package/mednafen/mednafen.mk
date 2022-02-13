ed8caa3040b8dcfd2f691fe7e8f4cb2f1f27992d
Daniel Palmer <daniel@0x0f.com>
2022-02-13 16:03:43 +0900

    package: add mednafen

---
+++ package/mednafen/mednafen.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# mednafen
+#
+################################################################################
+
+MEDNAFEN_VERSION = 1899500078169e787f86eb5302a3f35abdaa8764
+MEDNAFEN_SITE = https://github.com/libretro-mirrors/mednafen-git.git
+MEDNAFEN_SITE_METHOD = git
+MEDNAFEN_LICENSE = gpl-2.0
+MEDNAFEN_DEPENDS = sdl2
+MEDNAFEN_CONF_OPTS = --with-libflac=no
+
+$(eval $(autotools-package))
