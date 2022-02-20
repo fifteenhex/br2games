2626d0616b88adfa5f76a81438caf28f2e6b9616
Daniel Palmer <daniel@0x0f.com>
2022-02-20 23:06:00 +0900

    wip: vice
    Needs xa assembler

---
+++ package/vice/vice.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# vice
+#
+################################################################################
+
+VICE_VERSION = 3.6.1
+VICE_SITE = http://downloads.sourceforge.net/sourceforge/vice-emu
+VICE_SOURCE = vice-$(VICE_VERSION).tar.gz
+VICE_LICENSE = gpl-2.0
+VICE_DEPENDS = sdl2
+VICE_AUTORECONF = YES
+
+$(eval $(autotools-package))
