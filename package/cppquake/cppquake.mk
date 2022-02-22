3dc32d9d99e4f72267145cf174335cf7a2727ed6
Daniel Palmer <daniel@0x0f.com>
2022-02-22 18:31:47 +0900

    cppquake: Works, some visual glitches

---
+++ package/cppquake/cppquake.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# cppquake
+#
+################################################################################
+
+CPPQUAKE_VERSION = 201b54f9360de04a29fa001f655aaf9dcb16d106
+CPPQUAKE_SITE = https://github.com/linardsbi/cppquake.git
+CPPQUAKE_SITE_METHOD = git
+CPPQUAKE_DEPENDS = sdl2 libfmt libglm
+CPPQUAKE_LICENSE = GPL-2.0
+#CPPQUAKE_CONF_OPTS = -DUSE_DEBUGGERS=OFF -DUSE_GDB_STUB=OFF -DBUILD_QT=OFF -DUSE_SQLITE3=OFF -DUSE_DISCORD_RPC=OFF
+
+$(eval $(cmake-package))
