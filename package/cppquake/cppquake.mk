b2d9ccda1c041d686f1f23e14882a9114bf91aec
Daniel Palmer <daniel@0x0f.com>
2022-11-13 17:03:39 +0900

    pkg: cppquake: bump

--- package/cppquake/cppquake.mk
+++ package/cppquake/cppquake.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CPPQUAKE_VERSION = 201b54f9360de04a29fa001f655aaf9dcb16d106
+CPPQUAKE_VERSION = 44d859ff0b5f371a343b47540cf09d0e173631f2
 CPPQUAKE_SITE = https://github.com/linardsbi/cppquake.git
 CPPQUAKE_SITE_METHOD = git
 CPPQUAKE_DEPENDENCIES = sdl2 libfmt glm
