e38ddd5bab576b59d563d0f2f2730b0376e42fed
Daniel Palmer <daniel@0x0f.com>
2022-11-15 20:31:05 +0900

    pkg: cdogssdl: Starts building, needs ogl :(

--- package/cdogssdl/cdogs-sdl.mk
+++ package/cdogssdl/cdogs-sdl.mk
@@ -7,7 +7,7 @@
 CDOGSSDL_VERSION = 82a196d8f0d1a9563de61aa259ed51e52c90b4b2
 CDOGSSDL_SITE = https://github.com/cxong/cdogs-sdl.git
 CDOGSSDL_SITE_METHOD = git
-CDOGSSDL_DEPENDENCIES = sdl2
+CDOGSSDL_DEPENDENCIES = sdl2 sdl2_image sdl2_mixer
 CDOGSSDL_LICENSE = GPL-2.0
 
 $(eval $(cmake-package))
