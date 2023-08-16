7b265042fc49d8cb19db1c83afac0cde9dad4d71
Daniel Palmer <daniel@0x0f.com>
2023-08-16 18:03:25 +0900

    pkg: dosboxx: Builds now :)

--- package/dosboxx/dosboxx.mk
+++ package/dosboxx/dosboxx.mk
@@ -8,7 +8,8 @@ DOSBOXX_VERSION = 4327d58e75fe82829d594f2e43b32e7d7e78767c
 DOSBOXX_SITE = https://github.com/joncampbell123/dosbox-x.git
 DOSBOXX_SITE_METHOD = git
 DOSBOXX_LICENSE = gpl-2.0
-DOSBOXX_DEPENDENCIES = sdl2
+DOSBOXX_DEPENDENCIES = sdl2 zlib
 DOSBOXX_AUTORECONF = YES
+DOSBOXX_CONF_OPTS = "--enable-sdl2"
 
 $(eval $(autotools-package))
