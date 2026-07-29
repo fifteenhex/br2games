a0d5d337333068427f5a576938d6f1bfe301c59d
Daniel's intern <intern@thingy.jp>
2026-07-29 14:42:36 +0000

    package/vice: bump to 3.10
    Update VICE to 3.10 and add hash file with locally-computed sha256
    for the source tarball and COPYING.
    
    VICE 3.10 now checks for libcurl (WIC64 support) by default, which is
    not available in this toolchain; disable it with --without-libcurl.
    Explicitly select the SDL2 UI (--enable-sdl2ui) to match the SDL2 /
    SDL2_IMAGE dependencies. Builds cleanly for the arm target under
    GCC 15.3.0.

--- package/vice/vice.mk
+++ package/vice/vice.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-VICE_VERSION = 3.6.1
+VICE_VERSION = 3.10
 VICE_SITE = http://downloads.sourceforge.net/sourceforge/vice-emu
 VICE_SOURCE = vice-$(VICE_VERSION).tar.gz
-VICE_LICENSE = gpl-2.0
+VICE_LICENSE = GPL-2.0
+VICE_LICENSE_FILES = COPYING
 VICE_DEPENDENCIES = sdl2 sdl2_image host-dos2unix host-xa
 VICE_AUTORECONF = YES
-VICE_CONF_OPTS = --without-pulse --without-png
+VICE_CONF_OPTS = --without-pulse --without-png --without-libcurl --enable-sdl2ui
 
 $(eval $(autotools-package))
